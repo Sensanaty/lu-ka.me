8d66ac5ea5c47b95ac580598fc4a8314d130dbee
Sensananty <salevic@lu-ka.me>
2021-06-05 21:01:25 +0200

    Create the basic HTML and CSS skeleton for the homepage
    - Created the basic version of the checkerboard design
    - Adjusted the global heights of the html, body and #app selectors

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div id="home-wrapper">
+    <div class="home-box black" id="about-box">
+      <div class="big-letter" aria-hidden="true">L</div>
+      <router-link to="/about" class="white">ABOUT</router-link>
+    </div>
+    <div class="home-box white" id="projects-box">
+      <div class="big-letter" aria-hidden="true">U</div>
+      <router-link to="/projects" class="black">PROJECTS</router-link>
+    </div>
+    <div class="home-box white" id="contact-box">
+      <div class="big-letter" aria-hidden="true">K</div>
+      <router-link to="/contact" class="black">CONTACT</router-link>
+    </div>
+    <div class="home-box black" id="blog-box">
+      <div class="big-letter" aria-hidden="true">A</div>
+      <router-link to="/rambles" class="white">RAMBLES</router-link>
+    </div>
   </div>
 </template>
 
@@ -10,4 +25,30 @@
   };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  #home-wrapper {
+    height: 100%;
+    display: grid;
+    grid-template: repeat(2, 1fr) / repeat(2, 1fr);
+    user-select: none;
+
+    a {
+      text-decoration: none;
+      transition: color 110ms ease-in-out;
+
+      &:hover {
+        color: $accent;
+      }
+    }
+  }
+
+  .black {
+    background: $background;
+    color: $text;
+  }
+
+  .white {
+    background: $text;
+    color: $background;
+  }
+</style>
