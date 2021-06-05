4e8efd673a4f0daa4e7339d4b55c6e4eb58681bb
Sensananty <salevic@lu-ka.me>
2021-06-05 22:09:00 +0200

    Fix up the homepage styling & add some aria attributes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,19 +2,19 @@
   <div id="home-wrapper">
     <div class="home-box black" id="about-box">
       <span class="big-letter" aria-hidden="true">L</span>
-      <router-link to="/about" class="white">ABOUT</router-link>
+      <router-link to="/about" class="white" aria-label="About Luka">ABOUT</router-link>
     </div>
     <div class="home-box white" id="projects-box">
       <span class="big-letter" aria-hidden="true">U</span>
-      <router-link to="/projects" class="black">PROJECTS</router-link>
+      <router-link to="/projects" class="black" aria-label="Luka's Projects">PROJECTS</router-link>
     </div>
     <div class="home-box white" id="contact-box">
       <span class="big-letter" aria-hidden="true">K</span>
-      <router-link to="/contact" class="black">CONTACT</router-link>
+      <router-link to="/contact" class="black" aria-label="Luka's Contact Details">CONTACT</router-link>
     </div>
     <div class="home-box black" id="blog-box">
       <span class="big-letter" aria-hidden="true">A</span>
-      <router-link to="/rambles" class="white">RAMBLES</router-link>
+      <router-link to="/rambles" class="white" aria-label="Luka's Blog Posts">RAMBLES</router-link>
     </div>
   </div>
 </template>
@@ -29,20 +29,20 @@
   #home-wrapper {
     height: 100%;
     display: grid;
-    grid-template: repeat(2, 1fr) / repeat(2, 1fr);
+    grid-template: 1fr 1fr / 1fr 1fr;
     user-select: none;
 
     a {
-      font-size: 1.3rem;
+      font-size: 1.35rem;
       display: flex;
       align-items: center;
       justify-content: center;
       text-decoration: none;
-      transition: color 110ms ease-in-out;
+      transition: all 110ms ease-in-out;
       grid-area: link;
 
       /* prettier-ignore */
-      &:hover { color: $accent }
+      &:hover { color: darken($accent, 17) }
     }
   }
 
@@ -103,5 +103,10 @@
       "link . ."
       ". letter ."
       ". . .";
+  }
+
+  /* prettier-ignore */
+  #contact-box, #blog-box {
+    .big-letter { align-self: flex-start }
   }
 </style>
