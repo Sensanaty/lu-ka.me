002490bf2149ec93be41ca447bf6f558fea81f2e
Sensananty <salevic@lu-ka.me>
2021-06-05 21:33:21 +0200

    Touched up the checkerboard pattern on the homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="home-wrapper">
     <div class="home-box black" id="about-box">
-      <div class="big-letter" aria-hidden="true">L</div>
+      <span class="big-letter" aria-hidden="true">L</span>
       <router-link to="/about" class="white">ABOUT</router-link>
     </div>
     <div class="home-box white" id="projects-box">
-      <div class="big-letter" aria-hidden="true">U</div>
+      <span class="big-letter" aria-hidden="true">U</span>
       <router-link to="/projects" class="black">PROJECTS</router-link>
     </div>
     <div class="home-box white" id="contact-box">
-      <div class="big-letter" aria-hidden="true">K</div>
+      <span class="big-letter" aria-hidden="true">K</span>
       <router-link to="/contact" class="black">CONTACT</router-link>
     </div>
     <div class="home-box black" id="blog-box">
-      <div class="big-letter" aria-hidden="true">A</div>
+      <span class="big-letter" aria-hidden="true">A</span>
       <router-link to="/rambles" class="white">RAMBLES</router-link>
     </div>
   </div>
@@ -33,12 +33,16 @@
     user-select: none;
 
     a {
+      font-size: 1.3rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
       text-decoration: none;
       transition: color 110ms ease-in-out;
+      grid-area: link;
 
-      &:hover {
-        color: $accent;
-      }
+      /* prettier-ignore */
+      &:hover { color: $accent }
     }
   }
 
@@ -50,5 +54,54 @@
   .white {
     background: $text;
     color: $background;
+  }
+
+  .big-letter {
+    font-size: 15vh;
+    line-height: 1;
+    align-self: end;
+    justify-self: center;
+    grid-area: letter;
+  }
+
+  /* prettier-ignore */
+  .home-box { display: grid }
+
+  // Box Grids
+
+  // Top Left - L - ABOUT
+  #about-box {
+    grid-template: 1fr 1fr 130px / 3fr 1fr 130px;
+    grid-template-areas:
+      ". . ."
+      ". letter ."
+      ". . link";
+  }
+
+  // Top Right - U - PROJECTS
+  #projects-box {
+    grid-template: 1fr 1fr 130px / 130px 1fr 3fr;
+    grid-template-areas:
+      ". . ."
+      ". letter ."
+      "link . .";
+  }
+
+  // Bottom Left - K - CONTACT
+  #contact-box {
+    grid-template: 130px 1fr 1fr / 3fr 1fr 130px;
+    grid-template-areas:
+      ". . link"
+      ". letter ."
+      ". . .";
+  }
+
+  // Bottom Right - A - RAMBLES
+  #blog-box {
+    grid-template: 130px 1fr 1fr / 130px 1fr 3fr;
+    grid-template-areas:
+      "link . ."
+      ". letter ."
+      ". . .";
   }
 </style>
