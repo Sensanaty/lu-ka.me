d6c79791e3d752ffe0dd88b3c7c66e7dac0f8598
Sensananty <salevic@lu-ka.me>
2021-06-06 17:11:30 +0200

    Finish up HeaderNav styling

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <h1>Header Nav</h1>
-  </div>
+  <header>
+    <nav>
+      <router-link to="/" aria-label="Homepage">LUKA</router-link>
+      <router-link to="/projects" aria-label="Projects">PROJECTS</router-link>
+      <router-link to="/contact" aria-label="Contact">CONTACT</router-link>
+      <router-link to="/rambles" aria-label="Rambles">RAMBLES</router-link>
+      <router-link to="/about" aria-label="About">ABOUT</router-link>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -10,4 +16,31 @@
   };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  nav {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0 15px 0;
+
+    a {
+      text-decoration: none;
+      color: $accent;
+      background: lighten($background, 4);
+      font-size: 2.3rem;
+      padding: 5px 0;
+      min-width: 18%;
+      text-align: center;
+      letter-spacing: 2px;
+      border-top: 2px solid lighten($background, 4);
+      transition: transform 110ms ease-in-out, background 110ms ease-in-out, border-top-color 110ms ease-in-out;
+
+      &.router-link-active {
+        background: lighten($background, 8);
+        border-top: 2px solid $accent;
+      }
+
+      /* prettier-ignore */
+      &:hover { transform: translateY(2px) }
+    }
+  }
+</style>
