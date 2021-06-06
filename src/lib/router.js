93dd3fc20fb6005d0b4799e11080f6daaa404392
Sensananty <salevic@lu-ka.me>
2021-06-06 14:41:18 +0200

    Add 'About' page to router & adjust global text color style

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../pages/index.vue";
+import AboutPage from "../pages/about/index.vue";
 
 const routes = [
   {
     path: "/",
     component: HomePage,
     name: "home",
+  },
+  {
+    path: "/about",
+    component: AboutPage,
+    name: "about",
   },
 ];
 
