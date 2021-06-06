4179232e22d1be8fa65ed81f748ca6e8da4a866e
Sensananty <salevic@lu-ka.me>
2021-06-06 16:49:20 +0200

    Add the HeaderNav component to the project
    - Created the skeleton, with a title
    - Imported in App.vue, where it's conditionally displayed based on whether the current route is the correct one

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <HeaderNav v-if="showHeader" />
   <RouterView />
 </template>
 
 <script>
+  import HeaderNav from "./components/HeaderNav.vue";
+
   export default {
     name: "App",
+    components: {
+      HeaderNav,
+    },
+    computed: {
+      showHeader() {
+        const route = this.$route.name;
+        return !(route === "home" || route === "about" || !route);
+      },
+    },
   };
 </script>
 
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Header Nav</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HeaderNav",
+  };
+</script>
+
+<style lang="scss"></style>
