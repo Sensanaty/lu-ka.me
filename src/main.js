cd3f823edf0267618e324ada592844b47723559e
Sensananty <salevic@lu-ka.me>
2021-06-07 00:33:21 +0200

    Install axios to project & configure it in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./lib/router";
+import { $axios } from "./lib/axios";
 
 import "./assets/styles/global.scss";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App).use(router);
+app.config.globalProperties.$axios = $axios;
+
+app.mount("#app");
